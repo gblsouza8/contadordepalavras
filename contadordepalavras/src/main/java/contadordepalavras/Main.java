4a358ed8f90284df8ce4d0a96012537f18bca51f
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-10 19:50:41 -0300

    Segundo Commit, programa finalizado

--- contadordepalavras/src/main/java/contadordepalavras/Main.java
+++ contadordepalavras/src/main/java/contadordepalavras/Main.java
@@ -1,7 +1,19 @@
 package contadordepalavras;
-
+import java.util.Scanner;
 public class Main {
+    @SuppressWarnings({"unused", "ConvertToTryWithResources"})
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Coloque aqui o texto: ");
+        String texto = input.nextLine();
+        input.close();
+
+        String[] palavras = texto.split(" ");
+        int contador = 0;
+        for (String palavra : palavras) {
+            contador += 1;
+        }
+        System.out.println("Quantidade de palavras no texto: " + contador);
+
     }
 }
